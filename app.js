539af5f5eec53d0b70feb8671a2e7d977bd3f236
Rafael Pawlos <rafael.pawlos@gmail.com>
2017-06-15 17:24:19 +0200

    Improve data parsing

--- app.js
+++ app.js
@@ -20,38 +20,38 @@ var statLib = {
       return (prev < curr ? prev : curr);
     });
   },
-  
+
   max: function (values) {
     return values.reduce(function (prev, curr) {
       return (prev > curr ? prev : curr);
     });
   },
-  
+
   sum: function (values) {
     return values.reduce(function (prev, curr) {
       return curr += prev;
     });
   },
-  
+
   mean: function (values, sum) {
     var self = this;
-    
+
     var n = values.length;
     sum = (sum === void(0)) ? self.sum(values) : sum;
 
     return sum / n;
   },
-  
+
   median: function (values) {
-    values.sort(function(a, b){
-     return a - b;
+    values.sort(function (a, b) {
+      return a - b;
     });
-    
+
     var lowMiddle = Math.floor((values.length - 1) / 2);
     var highMiddle = Math.ceil((values.length - 1) / 2);
     return (values[lowMiddle] + values[highMiddle]) / 2;
   },
-  
+
   variance: function (values, mean) {
     var self = this;
 
@@ -66,19 +66,19 @@ var statLib = {
 
     return dev / n;
   },
-  
+
   stdDeviation: function (values, variance) {
     var self = this;
-    
-    variance = (variance === void(0)) ? self.variance(values) : variance; 
+
+    variance = (variance === void(0)) ? self.variance(values) : variance;
 
     return Math.sqrt(variance);
   },
-  
+
   uncertainty: function (values, variance) {
     var self = this;
-    
-    variance = (variance === void(0)) ? self.variance(values) : variance; 
+
+    variance = (variance === void(0)) ? self.variance(values) : variance;
     var n = values.length;
 
     return Math.sqrt((variance * n) / (n * (n - 1)));
@@ -91,14 +91,16 @@ var statCalc = {
     /*
      * opts = {
      *   input: (DOM object)
-     *   minOut: (DOM object)
-     *   maxOut: (DOM object)
-     *   sumOut: (DOM object)
-     *   meanOut: (DOM object)
-     *   medianOut: (DOM object)
-     *   varianceOut: (DOM object)
-     *   stdDeviationOut: (DOM object)
-     *   uncertaintyOut: (DOM object)
+     *   out:{
+     *     min: (DOM object)
+     *     max: (DOM object)
+     *     sum: (DOM object)
+     *     mean: (DOM object)
+     *     median: (DOM object)
+     *     variance: (DOM object)
+     *     stdDeviation: (DOM object)
+     *     uncertainty: (DOM object)
+     *   }
      * }
      */
     self.opts = opts;
@@ -106,44 +108,61 @@ var statCalc = {
     self.opts.input.addEventListener('input', function (e) {
       self.processData.call(self, e.target.value);
     }, false);
-    
+
     return self;
   },
-  
+
   processData: function (data) {
     var self = this;
 
     // Get array of values from string
-    var values = data.trim().split(/[\s\n]+/);
-    
-    // Convert array of string to array of floats
-    for (var i = 0; i < values.length; i++) {
-      values[i] = parseFloat(values[i]);  
+    var values = self.extractNumbers(data);
+
+    if (values === null) {
+      self.clear();
+      return;
     }
-    
+
     // Calculate stats
-    self.opts.minOut.value = statLib.min(values);
-    self.opts.maxOut.value = statLib.max(values);
-    self.opts.sumOut.value = statLib.sum(values);
-    self.opts.meanOut.value = statLib.mean(values, self.opts.sumOut.value);
-    self.opts.medianOut.value = statLib.median(values);
-    self.opts.varianceOut.value = statLib.variance(values, self.opts.meanOut.value);
-    self.opts.stdDeviationOut.value = statLib.stdDeviation(values, self.opts.varianceOut.value);
-    self.opts.uncertaintyOut.value = statLib.uncertainty(values, self.opts.varianceOut.value);
+    self.opts.out.min.value = statLib.min(values);
+    self.opts.out.max.value = statLib.max(values);
+    self.opts.out.sum.value = statLib.sum(values);
+    self.opts.out.mean.value = statLib.mean(values, self.opts.out.sum.value);
+    self.opts.out.median.value = statLib.median(values);
+    self.opts.out.variance.value = statLib.variance(values, self.opts.out.mean.value);
+    self.opts.out.stdDeviation.value = statLib.stdDeviation(values, self.opts.out.variance.value);
+    self.opts.out.uncertainty.value = statLib.uncertainty(values, self.opts.out.variance.value);
+  },
+
+  extractNumbers: (function () {
+    var numberPattern = /\-?\d+\.?\d*e\-?\+?\d+|\-?\d*\.?\d+/g;
+
+    return function (data) {
+      var numbers = data.match(numberPattern);
+      return (numbers !== null) ? numbers.map(Number) : null;
+    };
+  })(),
+
+  clear: function () {
+    var self = this;
+
+    Object.keys(self.opts.out).forEach(function (key) {
+      self.opts.out[key].value = '';
+    });
   }
 };
 
 var dictate = {
   init: function (opts) {
     var self = this;
-    
+
     self.opts = {
       // toggler: (DOM object),
       // input: (DOM object),
       continous: true,
       lang: 'pl-PL'
     };
-    
+
     tools.extend(self.opts, opts);
 
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
@@ -152,15 +171,15 @@ var dictate = {
     self.recognition.lang = self.opts.lang;
 
     self.recognition.onresult = self.onResult.bind(self);
-    self.recognition.onaudioend = function(){
+    self.recognition.onaudioend = function () {
       self.opts.toggler.checked = false;
     };
-    
+
     self.opts.toggler.addEventListener('change', self.toggle.bind(self), false);
 
     return self;
   },
-  
+
   toggle: function (e) {
     var self = this;
 
@@ -170,19 +189,19 @@ var dictate = {
       self.stop();
     }
   },
-  
+
   start: function () {
     var self = this;
 
     self.recognition.start();
   },
-  
+
   stop: function () {
     var self = this;
 
     self.recognition.stop();
   },
-  
+
   onResult: function (e) {
     var self = this;
 
@@ -199,7 +218,7 @@ var dictate = {
     }
 
     self.opts.input.value += transcript + ' ';
-    
+
     event = new Event('input');
     self.opts.input.dispatchEvent(event);
   }
@@ -207,14 +226,16 @@ var dictate = {
 
 statCalc.init({
   input: document.getElementById('inpuData'),
-  minOut: document.getElementById('minValue'),
-  maxOut: document.getElementById('maxValue'),
-  sumOut: document.getElementById('sumValue'),
-  meanOut: document.getElementById('meanValue'),
-  medianOut: document.getElementById('medianValue'),
-  varianceOut: document.getElementById('varianceValue'),
-  stdDeviationOut: document.getElementById('stdDeviationValue'),
-  uncertaintyOut: document.getElementById('uncertaintyValue')
+  out: {
+    min: document.getElementById('minValue'),
+    max: document.getElementById('maxValue'),
+    sum: document.getElementById('sumValue'),
+    mean: document.getElementById('meanValue'),
+    median: document.getElementById('medianValue'),
+    variance: document.getElementById('varianceValue'),
+    stdDeviation: document.getElementById('stdDeviationValue'),
+    uncertainty: document.getElementById('uncertaintyValue')
+  }
 });
 
 dictate.init({
